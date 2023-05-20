ec3cf1a1cbd47104c0a7446cb0ed8b58d22601bf
femas1 <federico.massara2@gmail.com>
2023-05-20 15:22:10 +0200

    Add error handling to fetch blogs

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,8 @@ function App() {
   
   return (
     <div className='app-container'>
-    
         <Navbar/>
         <BlogList/>
- 
     </div>
   )
 }
--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,4 +1,15 @@
-import { Heading, Text, Flex, Spinner, Link } from "@chakra-ui/react";
+import React from "react";
+import { 
+  Heading, 
+  Text, 
+  Flex, 
+  Spinner, 
+  Link,  
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react";
 import '../index.css' 
 import { useEffect, useState } from "react";
 
@@ -6,32 +17,44 @@ const BlogList = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-      fetch('http://localhost:3000/blogs')
-      .then(res => res.json())
+      fetch('http://localhost:3000/blsogs')
+      .then(res => {
+          if(!res.ok){
+            throw Error("I couldn't find the blogs")
+          } 
+          return res.json();
+      })
       .then(data => {
-            setBlogs(data)
-            setIsLoading(false)
-      });
+            setBlogs(data);
+            setIsLoading(false);
+            setError(null);
+      })
+      .catch((e) => {
+            setError(e.message);
+            setIsLoading(false);
+      })
   }, [])
 
   return (
     <div className="blog-list">
 
-      {isLoading && <Heading mt={10} p={5}>Loading blogs...    <Spinner size='xl' /></Heading>}
-
+            {error && <Alert status="error">
+                          <AlertIcon></AlertIcon>
+                          <AlertTitle>Houston, we've got a problem.</AlertTitle>
+                          <AlertDescription>{error}</AlertDescription>
+                            
+                      </Alert>}
+            {isLoading && <Heading mt={10} p={5}>Loading blogs...<Spinner size='xl' /></Heading>}
+            
             {blogs && blogs.map(blog => (
-                
-              
-                    <Flex key={blog.id} direction="column">
-                    
+                    <Flex key={blog.id} direction="column">                   
                       <Heading p="3">{blog.title}</Heading>
                       <Text p="5">{blog.text}</Text>
                       <Link p="3">{blog.author}</Link>
                     </Flex>
-             
-           
                 )
             )}
     </div>
