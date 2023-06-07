3160fb3859c910863d96b55beae2f21a5a17a18b
Federico Massara <federico.massara2@gmail.com>
2023-06-07 08:47:29 +0200

    add styling to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from "../assets/logo.png";
-import { Flex, Image, Box, Link } from '@chakra-ui/react';
-import { Link as RouteLink } from "react-router-dom";
+import { Image, Link, SimpleGrid } from '@chakra-ui/react';
 
 const Footer = () => {
   return (
     <>
-         <Flex 
+         <SimpleGrid 
+                columns={3} 
+                spacing={1}
                 p={5} 
                 mt={10}
                 bgColor="#FD0324"
-                align="center"
-                justify="space-between"
-                // maxWidth="1024px"
                 width="100%"
-                height="200px"
-
+                height="100px"
           >
-            <Image src={logo} maxHeight="80px" />
-          </Flex>
+            <Image 
+                src={logo} 
+                maxHeight="80px" 
+            />
+            <Link 
+                href='https://federico-massara.netlify.app/'
+                textAlign={"center"} 
+                pt={5}
+                isExternal
+            >
+              Federico Massara - 2023 &copy;
+            </Link>
+            
+            </SimpleGrid>
     </>
   )
 }
