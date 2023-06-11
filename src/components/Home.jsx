32df8ba9055f4e6903326d36b99d5941bde2d25a
Federico Massara <federico.massara2@gmail.com>
2023-06-11 14:38:07 +0200

    add cover photo to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import TextBox from './TextBox'
-import BlogDetails from './BlogDetails';
+import { Image, AspectRatio } from '@chakra-ui/react';
 import { heading, text } from '../homeContent';
 
 const Home = () => {
   return (
 <>
+
+    <Image 
+          width="100%"
+          height="300px"
+          objectFit='cover' 
+          src='src\assets\koeln-titelbild.jpg' alt='Dan Abramov' />
+
             <TextBox heading= {heading} text={<div dangerouslySetInnerHTML={{ __html: text }} />}/>
 </>
 )
